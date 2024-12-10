672058613f71791f6d92b692bb020aa03c773960
fernandogerson <fluqueg@unsa.edu.pe>
2024-12-10 06:43:52 -0500

    Ralizamos una inyeccion de SELECT a la db y lo desplegamos

--- cgi-bin/verPag.pl
+++ cgi-bin/verPag.pl
@@ -7,7 +7,7 @@ use DBI;
 my $cgi = CGI->new;
 my $page_id = $cgi->param('id');
 
-# Conexión a la base de datos
+# Conexion a la base de datos
 my $dsn = "DBI:mysql:database=markdown_db;host=midb;port=3306";
 my $db_user = "root";
 my $db_password = "1234";
@@ -17,3 +17,5 @@ my $dbh = DBI->connect($dsn, $db_user, $db_password, {
     RaiseError => 1,
     AutoCommit => 1,
 }) or die "No se pudo conectar a la base de datos: $DBI::errstr";
+my $sth = $dbh->prepare("SELECT name, content FROM pages WHERE id = ?");
+$sth->execute($page_id);
