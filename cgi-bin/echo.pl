1e8add7d51d811c5e1ee9655c78a4132cdc3fb87
fernandogerson <fluqueg@unsa.edu.pe>
2024-12-10 06:24:38 -0500

    Creando la conexion a la base de datos

--- cgi-bin/echo.pl
+++ cgi-bin/echo.pl
@@ -1,1 +1,18 @@
 #!/usr/bin/perl
+
+# Echo: creara las paginas
+use strict;
+use warnings;
+use CGI;
+use DBI;
+
+my $cgi = CGI->new;
+
+my $dsn = "DBI:mysql:database=markdown_db;host=midb;port=3306";
+my $db_user = "root";
+my $db_password = "1234";
+
+my $dbh = DBI->connect($dsn, $db_user, $db_password, {
+    RaiseError => 1,
+    AutoCommit => 1,
+}) or die "No se pudo conectar a la base de datos: $DBI::errstr";
