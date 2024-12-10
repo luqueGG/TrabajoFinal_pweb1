e113b867520c59bf13fe771e1a20c60a986fff36
fernandogerson <fluqueg@unsa.edu.pe>
2024-12-10 06:38:11 -0500

    Estableciendo Conexion a la base de datos

--- cgi-bin/get_pages.pl
+++ cgi-bin/get_pages.pl
@@ -6,3 +6,12 @@ use DBI;
 use JSON;
 
 my $cgi = CGI->new;
+
+my $dsn = "DBI:mysql:database=markdown_db;host=midb;port=3306";
+my $db_user = "root";
+my $db_password = "1234";
+
+my $dbh = DBI->connect($dsn, $db_user, $db_password, {
+    RaiseError => 1,
+    AutoCommit => 1,
+}) or die "No se pudo conectar a la base de datos: $DBI::errstr";
